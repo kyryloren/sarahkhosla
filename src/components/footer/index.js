94c1546be0c15205148374a0bf45a0d9bd9fdc96
Kyrylo Orlov <dev@kyryloorlov.com>
2021-05-10 11:58:00 -0400

    Hooked up footer to CMS

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
 import { Icon } from '@components';
 import { FooterWrapper, Row, Col, Year, FooterLink, MobileCol, InformationLink } from './style';
 import { Container } from '@styles';
 
 const Footer = () => {
+  const query = useStaticQuery(graphql`
+    query {
+      allPrismicInformation {
+        edges {
+          node {
+            data {
+              footer {
+                link {
+                  url
+                  target
+                }
+                text
+              }
+            }
+          }
+        }
+      }
+    }
+  `);
   let currentYear = new Date().getFullYear();
+  const doc = query.allPrismicInformation.edges.slice(0, 1).pop();
+  const data = doc.node.data;
 
   return (
     <FooterWrapper>
@@ -15,21 +37,17 @@ const Footer = () => {
           </Col>
           <Col right>
             <Row>
-              <Row>
-                <Icon name="arrow" />
-                <FooterLink href="https://linkedin.com" target="_blank" rel="noopener” “noreferrer">
-                  LinkedIn
-                </FooterLink>
-              </Row>
-              <Row>
-                <Icon name="arrow" />
-                <FooterLink
-                  href="https://instagram.com"
-                  target="_blank"
-                  rel="noopener” “noreferrer">
-                  Instagram
-                </FooterLink>
-              </Row>
+              {data.footer.map((data, index) => (
+                <Row>
+                  <Icon name="arrow" />
+                  <FooterLink
+                    href={data.link.url && data.link.url}
+                    target={data.link.target}
+                    rel="noopener noreferrer">
+                    {data.text}
+                  </FooterLink>
+                </Row>
+              ))}
               <Row>
                 <Icon name="arrow" />
                 <FooterLink
