166bed6d9a2147c5c5abdf8f66ce364b6d25c26e
Kyrylo Orlov <dev@kyryloorlov.com>
2021-05-16 16:04:38 -0400

    Updated transition delay on mobile

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,18 +2,22 @@ import React from 'react';
 import { Layout } from './src/components';
 import './src/styles/scroll.css';
 
-const transitionDelay = 100;
-
 export const shouldUpdateScroll = ({ routerProps: { location }, getSavedScrollPosition }) => {
+  // transition duration from `layout.js` * 1000 to get time in ms
+  // * 2 for exit + enter animation
+  const TRANSITION_DELAY = 0.5 * 1000 * 2;
+  // if it's a "normal" route
   if (location.action === 'PUSH') {
-    window.setTimeout(() => window.scrollTo(0, 0), transitionDelay);
-    document.getElementById('___container').scrollIntoView();
-  } else {
-    window.setTimeout(() => window.scrollTo(0, 0), transitionDelay);
-    document.getElementById('___container').scrollIntoView();
+    window.setTimeout(() => window.scrollTo(0, 0), TRANSITION_DELAY);
+  }
+  // if we used the browser's forwards or back button
+  else {
+    const savedPosition = getSavedScrollPosition(location) || [0, 0];
+    window.setTimeout(() => window.scrollTo(...savedPosition), TRANSITION_DELAY);
   }
   return false;
 };
+
 
 export const onClientEntry = () => {
   // IntersectionObserver polyfill for gatsby-background-image (Safari, IE)
